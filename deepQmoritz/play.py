92e0f71f664f18e4bbf9ac172fdfa92f28b64b2d
Moritz Eck <moritz.eck@gmail.com>
2019-05-03 16:44:01 +0200

    add: deep q-learning implemenation + first trial run

---
+++ deepQmoritz/play.py
@@ -0,0 +1,6 @@
+from agent import *
+
+# create DeepQ Agent with given environment
+agent = Agent('Test')
+# play the game
+agent.play()
---
+++ deepQmoritz/train.py
@@ -0,0 +1,6 @@
+from agent import *
+
+# create DeepQ Agent with given environment
+agent = Agent('Test2')
+# play the game
+agent.train()
